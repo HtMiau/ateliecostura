d012c7a6c248ffb87bf16b569b74cc8d4dc76957
HtMiau <davig4560@gmail.com>
2023-09-28 21:56:00 -0300

    primeiro commit

---
+++ Ani.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function() {
+        var menu = document.querySelector("header");
+        var scrollPosition = window.scrollY;
+
+        if (scrollPosition > 0) {
+            menu.classList.add("scrolled");
+        } else {
+            menu.classList.remove("scrolled");
+        }
+});
+
+
